124134d2be11bb36740583851dee6564040d3517
MotyaDev <137160953+MotyaDev@users.noreply.github.com>
2025-03-07 11:02:40 +0300

    BLIN

--- install.bat
+++ install.bat
@@ -16,14 +16,14 @@ rem Utils\\busybox wget https://github.com/scode18/All-Tweaker/raw/main/elevator
 rem Utils\\busybox wget https://github.com/scode18/All-Tweaker/raw/main/launcher.exe
 
 REM Создаем ярлык на рабочем столе с иконкой icon.ico
-echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
-echo sLinkFile = oWS.ExpandEnvironmentStrings("%USERPROFILE%\Desktop\All Tweaker.lnk") >> CreateShortcut.vbs
-echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
-echo oLink.TargetPath = "%CD%\All.Tweaker.Start.bat" >> CreateShortcut.vbs
-echo oLink.IconLocation = "%CD%\icon.ico" >> CreateShortcut.vbs
-echo oLink.WorkingDirectory = "%CD%" >> CreateShortcut.vbs
-echo oLink.Save >> CreateShortcut.vbs
-cscript CreateShortcut.vbs
+REM echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
+REM echo sLinkFile = oWS.ExpandEnvironmentStrings("%USERPROFILE%\Desktop\All Tweaker.lnk") >> CreateShortcut.vbs
+REM echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
+REM echo oLink.TargetPath = "%CD%\All.Tweaker.Start.bat" >> CreateShortcut.vbs
+REM echo oLink.IconLocation = "%CD%\icon.ico" >> CreateShortcut.vbs
+REM echo oLink.WorkingDirectory = "%CD%" >> CreateShortcut.vbs
+REM echo oLink.Save >> CreateShortcut.vbs
+REM cscript CreateShortcut.vbs
 
 REM Удаляем временный файл CreateShortcut.vbs
 del CreateShortcut.vbs
